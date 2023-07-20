bd1fbe82572a880ade3f3d5dfd639bf6b404b329
Prasad Mule <prasadmule870@gmail.com>
2023-07-20 12:22:07 +0530

    Committed

---
+++ src/main/java/com/example/SpringJenkinsProject/SpringJenkinsProjectApplication.java
@@ -0,0 +1,15 @@
+package com.example.SpringJenkinsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsProjectApplication {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Application Started");
+		SpringApplication.run(SpringJenkinsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringJenkinsProject/SpringJenkinsProjectApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.SpringJenkinsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Testing Done");
+	}
+
+}
